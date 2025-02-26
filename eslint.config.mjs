cc511fa22163f4f36b4ed5c66f754f12ce5ec036
jingeon27 <jingeon27@gmail.com>
2025-02-26 22:48:52 +0900

    eslint prettier 변경

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals", // Next.js 기본 규칙
+    "next/typescript", // Next.js + TypeScript 지원
+    "prettier", // Prettier와 충돌 방지
+  ),
+  {
+    plugins: ["prettier"], // Prettier 플러그인 추가
+    rules: {
+      "prettier/prettier": ["error", { singleQuote: true, semi: true }], // Prettier 스타일 적용
+      "react-hooks/exhaustive-deps": "off", // React Hooks 의존성 검사 비활성화
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/util/compareId.ts
+++ src/util/compareId.ts
@@ -1,4 +1,6 @@
-type returnParamType<T extends Object> = { id: number | string } & { [key in keyof T]: T[key] }
+type returnParamType<T extends Object> = { id: number | string } & {
+  [key in keyof T]: T[key];
+};
 
 /**
  * @example 오브젝트.findIndex(compareIndex(비교값))
@@ -6,8 +8,9 @@ type returnParamType<T extends Object> = { id: number | string } & { [key in key
  * @param isSame compareValue와 오브젝트 안의 값이 참인지 거짓인지 비교
  * @returns
  */
-export function compareId
-    (compareValue?: number | string, isSame = true) {
-    return <T extends Object>({ id }:returnParamType<T>)  =>
-        isSame ? String(compareValue) === String(id) : String(compareValue) !== String(id)
+export function compareId(compareValue?: number | string, isSame = true) {
+  return <T extends Object>({ id }: returnParamType<T>) =>
+    isSame
+      ? String(compareValue) === String(id)
+      : String(compareValue) !== String(id);
 }
