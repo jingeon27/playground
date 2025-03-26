92a140281a18c9ee8dba128feb85c4a964271075
gun kim <gun@whattime.co.kr>
2025-03-26 22:00:08 +0900

    typescript 버전 업

--- src/util/isStringId.ts
+++ src/util/isStringId.ts
@@ -1,6 +1,3 @@
 export function isStringId(param: { id: unknown }): param is { id: string } {
   return typeof param.id === "string";
 }
-export function isStringIds(...objs: { id: unknown }[]) {
-  return objs.every(isStringId);
-}
