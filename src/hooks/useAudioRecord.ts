fddf6da264219dce172c7209747b5f942feb660a
jingeon27 <jingeon27@gmail.com>
2025-03-28 22:19:09 +0900

    audio

---
+++ src/hooks/useAudioRecord.ts
@@ -0,0 +1,41 @@
+import { useRef } from "react";
+
+export function useAudioRecord() {
+  const [isRecording, setIsRecording] = useState(false);
+  const [audioUrl, setAudioUrl] = useState<string | null>(null);
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const audioChunksRef = useRef<Blob[]>([]);
+
+  const startRecording = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorderRef.current = mediaRecorder;
+
+    mediaRecorder.ondataavailable = (event) => {
+      audioChunksRef.current.push(event.data);
+    };
+
+    mediaRecorder.onstop = () => {
+      const audioBlob = new Blob(audioChunksRef.current, { type: "audio/wav" });
+      const url = URL.createObjectURL(audioBlob);
+      setAudioUrl(url);
+      audioChunksRef.current = [];
+    };
+
+    mediaRecorder.start();
+    setIsRecording(true);
+  };
+
+  const stopRecording = () => {
+    mediaRecorderRef.current?.stop();
+    setIsRecording(false);
+  };
+  return {
+    isRecording,
+    stopRecording,
+    startRecording,
+    audioChunksRef,
+    mediaRecorderRef,
+    audioUrl,
+  };
+}
